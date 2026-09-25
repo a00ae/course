575525976ffe584305c40e0cf30b736bddb8573e
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-09-25 22:50:02 +0300

    fix 20

--- problem/problem20to32.cpp
+++ problem/problem20to32.cpp
@@ -10,6 +10,44 @@ struct stDate {
 	short Day;
 };
 
+
+bool IsDate1BeforeDate2(stDate Date1, stDate Date2)
+{
+	return (Date1.Year < Date2.Year) ? true : ((Date1.Year ==
+		Date2.Year) ? (Date1.Month < Date2.Month ? true : (Date1.Month ==
+			Date2.Month ? Date1.Day < Date2.Day : false)) : false);
+}
+
+
+bool IsLastDayInMonth(stDate Date) {
+
+	return  (Date.Day == NumberOfDaysInAMonth(Date.Month, Date.Year));
+
+}
+
+bool IsLastMinthInYear(short Month) {
+	return (Month == 12);
+}
+
+stDate IncreaseDateByOneDay(stDate Date) {
+	if (IsLastDayInMonth(Date)) {
+
+		if (IsLastMinthInYear(Date.Month)) {
+			Date.Month = 1;
+			Date.Day = 1;
+			Date.Year++;
+		}
+		else {
+			Date.Month++;
+			Date.Day = 1;
+		}
+	}
+	else {
+		Date.Day++;
+	}
+	return Date;
+}
+
 stDate ReadFullDate() {
 	stDate Date;
 
@@ -24,9 +62,12 @@ stDate ReadFullDate() {
 
 void problem20To32() {
 	stDate Date = ReadFullDate();
+	Date = IncreaseDateByOneDay(Date);
+
 
 
 	cout << "Date: " << Date.Day << "/" << Date.Month << "/" << Date.Year;
+
 
 
 }
